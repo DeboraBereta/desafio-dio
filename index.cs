72282e21c0557d96af9130b8c6ee5d8f462087e6
Débora Bereta <brusck2003@gmail.com>
2023-09-19 15:10:46 -0300

    projeto criado

---
+++ index.cs
@@ -0,0 +1,18 @@
+//vetor armazenando dados.
+var pokemon = new []{
+    new[]{$"POOCHVRNA", "LV{2}" ,"Força: {13/13}"},
+    new[]{$"ZIGZAGOON", "LV{2}" ,"Força: {13/13}"},
+    new[]{$"DRAGONITE", "LV{5}" ,"Força: {25/25}"},
+    new[]{$"DRAGONITE", "LV{5}" ,"Força: {24/24}"},
+    new[]{$"DRAGONITE", "LV{5}" ,"Força: {24/24}"},
+    new[]{$"POOCHVRNA", "LV{3}" ,"Força: {15/15}"},
+    new[]{$"WURMPLE", "LV{2}" ,"Força: {7/10}"}
+    };
+//exibe mensagem 
+Console.WriteLine( $"{pokemon[0][0]}, Cadastrado com sucesso!");
+Console.WriteLine( $"{pokemon[1][0]}, Cadastrado com sucesso!");
+Console.WriteLine( $"{pokemon[2][0]}, Cadastrado com sucesso!");
+Console.WriteLine( $"{pokemon[3][0]}, Cadastrado com sucesso!");
+Console.WriteLine( $"{pokemon[4][0]}, Cadastrado com sucesso!");
+Console.WriteLine( $"{pokemon[5][0]}, Cadastrado com sucesso!");
+Console.WriteLine( $"{pokemon[6][0]}, Cadastrado com sucesso!");
